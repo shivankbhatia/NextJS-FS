f51d5f7333fba6620e0c6accaf6bbd87b02ce39b
shivankbhatia <157305241+shivankbhatia@users.noreply.github.com>
2025-06-21 09:49:43 +0530

    Include NextAuth Files

--- nextjsfullstack/lib/auth.ts
+++ nextjsfullstack/lib/auth.ts
@@ -64,5 +64,17 @@ export const authOptions: NextAuthOptions = {
             }
             return session;
         },
-    }
+        async redirect({ url, baseUrl }) {
+            return baseUrl;
+        }
+    },
+    pages: {
+        signIn: "/login",
+        error: "/login",
+    },
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60
+    },
+    secret: process.env.NEXTAUTH_SECRET
 };
